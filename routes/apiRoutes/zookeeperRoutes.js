a9878d7be3c08830176ca13786e8334209b17e50
Steven <stevenbmendoza@gmail.com>
2022-04-12 19:37:19 -0700

    routes created for zookeeper api

---
+++ routes/apiRoutes/zookeeperRoutes.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+
+const { filterByQuery, findById, createNewZookeeper, validateZookeeper } = require('../../lib/zookeepers.js') 
+const { zookeepers } = require('../../data/zookeepers.json');
+
+// GET Routes for API
+router.get('/zookeepers', (req, res) => {
+  let results = zookeepers;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
+
+router.get('/zookeepers/:id', (req, res) => {
+  const result = findById(req.params.id, zookeepers);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
+// POST Routes
+router.post('/zookeepers', (req, res) => {
+  req.body.id = zookeepers.length.toString();
+  if (!validateZookeeper(req.body)) {
+    res.status(404).send('The zookeeper is not properly formatted.');
+  } else {
+    const zookeeper = createNewZookeeper(req.body, zookeepers);
+    res.json(zookeeper);
+  }
+});
+
+module.exports = router;
